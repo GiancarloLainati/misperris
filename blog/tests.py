64f89c49a404fb18f927b24f4c421f6a0b3e67e7
Giancarlo <giancarlo.lainati@gmail.com>
2018-10-31 01:37:38 -0300

    diseño finalizado

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Rescatado
+from django.utils import timezone
 
-# Create your tests here.
+""" class RescatadoTestCase(TestCase):
+    def setUp(self):
+        Rescatado.objects.create(fotografia="null", nombre="flor", raza="lola", descripcion="hola", estado="disponible", published_date=timezone.now)
+        Rescatado.objects.create(fotografia="null", nombre="pinta", raza="quilter", descripcion="holanda", estado="disponible", published_date=timezone.now)
+
+    def test_rescatado_nombre(self):
+        flor = Rescatado.objects.get(nombre="flor")
+        pinta = Rescatado.objects.get(nombre="pinta")
+        self.assertEqual(flor, 'flor')
+        self.assertEqual(pinta, 'pinta') """

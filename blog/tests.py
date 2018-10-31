5b44c9e5871d9fd179caea467715db3e4fcadb61
Giancarlo <giancarlo.lainati@gmail.com>
2018-10-31 17:28:46 -0300

    pruebas actualizadas

--- blog/tests.py
+++ blog/tests.py
@@ -1,14 +1,39 @@
-from django.test import TestCase
+import unittest
+from django.test import Client
 from .models import Rescatado
 from django.utils import timezone
 
-""" class RescatadoTestCase(TestCase):
-    def setUp(self):
-        Rescatado.objects.create(fotografia="null", nombre="flor", raza="lola", descripcion="hola", estado="disponible", published_date=timezone.now)
-        Rescatado.objects.create(fotografia="null", nombre="pinta", raza="quilter", descripcion="holanda", estado="disponible", published_date=timezone.now)
+# Para ejecutar el test colocar python manage.py test
 
-    def test_rescatado_nombre(self):
-        flor = Rescatado.objects.get(nombre="flor")
-        pinta = Rescatado.objects.get(nombre="pinta")
-        self.assertEqual(flor, 'flor')
-        self.assertEqual(pinta, 'pinta') """
+class SimpleTest(unittest.TestCase):
+   def setUp(self):
+       # Todo test necesita un cliente.
+       self.client = Client()
+
+   def test_details_login(self):
+       # Se emite una solicitud GET.
+       response = self.client.get('/login/')
+
+       # Se comprueba que la respuesta es 200.
+       self.assertEqual(response.status_code, 200)
+  
+   def test_details_signup(self):
+       # Se emite una solicitud GET.
+       response = self.client.get('/signup/')
+
+       # Se comprueba que la respuesta es 200.
+       self.assertEqual(response.status_code, 200)
+
+   def test_details_rescatados(self):
+       # Se emite una solicitud GET.
+       response = self.client.get('/rescatados/')
+
+       # Se comprueba que la respuesta es 200.
+       self.assertEqual(response.status_code, 200)
+
+   def test_details_password_reset(self):
+       # Se emite una solicitud GET.
+       response = self.client.get('/password_reset/')
+
+       # Se comprueba que la respuesta es 200.
+       self.assertEqual(response.status_code, 200)
